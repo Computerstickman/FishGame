a4629eda216ca5cf5e480f6a4bf2580cca2039b2
Juliet Gemmell <jegemmel@ucsc.edu>
2022-05-04 12:40:03 -0700

    Added some menu visuals and preloaded sounds

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,12 +5,30 @@ class Menu extends Phaser.Scene {
 
     preload(){
         //preload menu assets and sound effects here
+        this.load.audio('movement', './assets/movementnoise.wav');
+        this.load.audio('damage', './assets/damagenoise.wav');
+        this.load.audio('death', './assets/deathnoise.wav');
+        this.load.audio('sharkroar', './assets/beastroar.wav');
+        this.load.image('background', './assets/oceanbackground.png');
     }
 
     create(){
         //text config if applicable
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#38939d',
+            color: '#ffffff',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
 
         //menu text
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'Press ENTER to Start Tutorial', menuConfig).setOrigin(0.5);
 
         //define keys;
         keyENTER = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER)
@@ -25,6 +43,7 @@ class Menu extends Phaser.Scene {
             //play enter sound
             //skips intro
         this.scene.start("tutorialScene");
+        this.sound.play('movement');
         }
     }
 
